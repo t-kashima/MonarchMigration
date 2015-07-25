574ba33cd8d1221088783f5c2fb89a205eaf8aa1
Kashima Takumi <kodokurider@gmail.com>
2015-07-25 21:03:21 +0900

    カラムを追加したり削除できるマイグレーションファイルを作れるようになりました

--- lib/monarch_migration.rb
+++ lib/monarch_migration.rb
@@ -17,31 +17,51 @@ end
 
 module MonarchMigration
   class CLI < Thor
-    desc "generate action_and_class_name column:type...", "generate migration file."
+    desc "generate action_and_class_name column_list", "generate migration file."
     def generate(action_and_class_name, *column_list)
       unixtime = Time.now.to_i
-      unixtime = 1437816936
       filename = "#{unixtime}_#{action_and_class_name.snakecase}.json"
-      match_list = action_and_class_name.match(%r{addColumn(.+)})
-      class_name = match_list[1]
+      match_list = action_and_class_name.match(%r{(.+?)Column(.+)})
+      action = match_list[1]
+      class_name = match_list[2]
+      puts "action: #{action}"
       puts "class name: #{class_name}"
       puts "filename: #{filename}"
       puts "column: #{column_list}"
-      # migration column and type
-      up_list = []
-      column_list.each{|column|
-        up = "addcolumn #{class_name} #{column}"
-        up_list.push(up)
-      }
+
+      if action == "add" then
+        up_list = get_add_column_up_list(class_name, column_list)
+      elsif action == "remove"
+        up_list = get_remove_column_up_list(class_name, column_list)
+      end
+
       json_object = {}
       json_object["up"] = up_list
       json_generate(filename, json_object)
     end
 
     private
+    def get_add_column_up_list(class_name, column_list)
+      up_list = []
+      column_list.each{|column|
+        up = "addcolumn #{class_name} #{column}"
+        up_list.push(up)
+      }
+      return up_list
+    end
+
+    def get_remove_column_up_list(class_name, column_list)
+      up_list = []
+      column_list.each{|column|
+        up = "removecolumn #{class_name} #{column}"
+        up_list.push(up)
+      }
+      return up_list
+    end
+
     def json_generate(filename, object)
       open(filename, "w") do |io|
-        JSON.dump(object, io)
+        io.write(object.to_json)
       end
     end
   end
